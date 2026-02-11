7f562d80003e4873b12e786a5b291e328cab386f
Karolis Zabulis <karolis.zabulis@mif.stud.vu.lt>
2026-02-11 19:05:30 +0200

    Adding env.example and setup script update

--- src/scripts/web_setup.sh
+++ src/scripts/web_setup.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
+set -e
 
 apt install nginx -y
 /bin/bash -c "$(curl -fsSL https://php.new/install/linux/8.4)"
-apt install php-fpm npm php8.4.xml php8.4-sqlite3 -y
-cp -r ~/webshark/src/WebShark /var/www/WebShark
+apt install php-fpm npm php8.4-xml php8.4-sqlite3 php-mbstring -y
 
-cp ~/webshark/src/scripts/nginx_conf/WenShark /ect/nxing/sites-available/
+cp ./nginx_conf/WebShark /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/WebShark /etc/nginx/sites-enabled/
 rm /etc/nginx/sites-enabled/default
 
+cp -r ../WebShark /var/www/WebShark
 cd /var/www/WebShark
-composer install
+cp ../WebShark/.env.example /var/www/WebShark/.env
+~/.config/herd-lite/bin/composer install
 php artisan key:generate
 npm install
 npm run build
 
-chown -r www-data:www-data /var/www/WebShark
-chmod -r 755 /var/www/WebShark
+chown -R www-data:www-data /var/www/WebShark
+chmod -R 755 /var/www/WebShark
 
 systemctl restart php8.4-fpm
 systemctl reload nginx
