14103cd23ae8d225f7282b2b9d1e360f8398bcb2
Karolis Zabulis <karolis.zabulis@mif.stud.vu.lt>
2026-02-17 17:23:33 +0200

    File upload and home UI
    - Uploaded files are now validated.
    - Uploaded files are stored inside storage/app/private/pcap directory, the name is combined of current session and the original name.
    - Minimalistic UI for MVP
    - Validation messages based on backend response

--- src/WebShark/app/Http/Controllers/FileController.php
+++ src/WebShark/app/Http/Controllers/FileController.php
@@ -4,18 +4,43 @@ namespace App\Http\Controllers;
 
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
 
 class FileController extends Controller
 {
     public function uploadPcap(Request $request){
         $validated = $request->validate([
-            // Laravel does not distinguish mime type pcap,pcapng and returns an error another validation way is needed
-            // 'pcap_file' => 'required|mimes:pcap,pcapng|max:102400'
-            'pcap_file' => 'required|file|max:102400'
+            // Now the file size is limited to 10 mb
+            'pcap_file' => 'required|file|max:10240'
         ]);
-        return response()->json([
-            'succeess' => true,
-            'message' => 'File uploaded successfully'
-        ]);
+
+        $sessionID = session()->getId();
+
+        // Laravel does not distinguish mime type pcap,pcapng and returns an error therefore, a manual check is needed
+        if(!in_array($request->file('pcap_file')->getClientOriginalExtension(), ['pcap', 'pcapng'])){
+            return response()->json([
+                'error' => 'File upload failed, incorrect file extension'
+            ], 422);
+        }
+
+        try {
+            // Preserving the pcap/pcapng extension, because laravel does not
+            $fileName = $request->file('pcap_file')->getClientOriginalName();
+            $rebuiltFileName = $sessionID . "_" . $fileName;
+            $path = $request->file('pcap_file')->storeAs('pcap', $rebuiltFileName);
+
+            return response()->json([
+                'success' => 'File upload was successful.',
+            ]);
+        } catch (Exception $e) {
+            Log::error("File save failed", [
+                'message' => $e->getMessage(),
+                'exception' => $e
+            ]);
+            return response()->json([
+                'error' => 'File upload failed'
+            ], 422);
+        }
     }
 }
--- src/WebShark/resources/js/pages/Home.vue
+++ src/WebShark/resources/js/pages/Home.vue
@@ -1,39 +1,53 @@
 <script setup>
-    import axios from 'axios'
-    import { ref } from 'vue'
+    import axios from 'axios';
+    import { ref } from 'vue';
 
     const file = ref(null);
+    const successMessage = ref(null);
+    const failureMessage = ref(null);
 
     const fileChange = (event) =>{
-        file.value = event.target.files[0]
-    }
+        file.value = event.target.files[0];
+    };
 
     const uploadFile = async() => {
         if(!file.value){
-            return
+            return;
         }
 
         try {
             // Form HTML data send
             const fileSendData = new FormData();
-            fileSendData.append('pcap_file', file.value)
+            fileSendData.append('pcap_file', file.value);
 
             const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/file/uploadPcap`,
                 fileSendData
             );
-            console.log(response.data);         
-        } catch (error){
-            console.error(error);
-        }
+            successMessage.value = response.data.success;
+            failureMessage.value = null;
 
+            
+        } catch (error){
+            console.log(error);
+            failureMessage.value = "File upload failed";
+            successMessage.value = null;
+        }
     }
 
 </script>
 
 <template>
     <div class = "flex-auto">
+        <!-- Feedback messages -->
+         <div v-if="failureMessage" class="bg-red-100 border border-red-400 text-center text-red-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">{{ failureMessage }}</strong>
+        </div>
+        <div v-if="successMessage" class = "bg-green-100 border border-green-400 text-center text-green-700 px-4 py-3 rounded relative" reole="allert">
+            <strong class="font-bold">{{ successMessage }}</strong>
+        </div>
+
         <div class = "spacer"></div>
-        <!-- File upload form -->
+        <!-- File upload container -->
         <div class = "file-container">
             <!-- Logo -->
             <div class = "flex justify-center items-center">
@@ -41,24 +55,17 @@
             </div>
             <div class = "flex justify-center items-center">
                 <div class = "file-upload">
-                    <!-- This form action needs to be rewritten with axio and CSRF tokens, currently won't work -->
                     <form @submit.prevent="uploadFile" enctype="multipart/form-data">
                         <div class="col-span-full py-2">
-                            <label for="cover" class="block text-center text-lg font-medium ">Analyse network traffic files, visualize important data, <br> spot potential vulnerabilities or attacks in your network.</label>
-                                <div class="mt-2 flex justify-center px-6 py-6 border border-dashed border-white/25">
-                                    <div class="text-center">
-                                        <div class="mt-4 flex text-sm/6 text-gray-400">
-                                                <label for="file-upload" class="relative cursor-pointer rounded-md bg-transparent font-semibold text-indigo-400 focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-indigo-500 hover:text-indigo-300">
-                                                    <span>Upload a file</span>
-                                                    <input id="file-upload" name="pcap_file" type="file" class="sr-only" @change="fileChange" required/>
-                                                </label>
-                                            <p class="pl-1">or drag and drop</p>
-                                        </div>
-                                        <p class="text-xs/5 text-gray-400">PCAP, PCAPNG up to 10MB</p>
-                                    </div>
+                            <label for="cover" class="block text-center text-lg font-medium">Analyse network traffic files, visualize important data, <br> spot potential vulnerabilities or attacks in your network.</label>
+                                <!-- Upload box -->
+                                <div class="upload-text py-4 text-center w-100 mx-auto">
+                                    <label class="text-base font-medium mb-3 block">Upload file</label>
+                                    <input @change="fileChange" type="file" class="w-full font-medium text-sm bg-white border file:cursor-pointer cursor-pointer file:border-0 file:py-3 file:px-4 file:mr-4 file:bg-gray-100 file:hover:bg-gray-200 file:text-slate-500 rounded" />
+                                    <p class="text-xs mt-2">PCAP and PCAPNG are allowed up to 10 MB.</p>
                                 </div>
                         </div>
-                        <div class="mt-6 flex items-center justify-center gap-x-6">
+                        <div class="flex items-center justify-center gap-x-6">
                             <button type="submit" class="rounded-md bg-indigo-500 px-6 py-2 text-md font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Analyze</button>
                         </div>
                     </form>
