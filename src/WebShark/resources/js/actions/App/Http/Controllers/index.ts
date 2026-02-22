f3216640e8c6bf02d142339501c3b50bd2bcc52b
Šerelis Lukas <lukas.serelis@mif.stud.vu.lt>
2026-02-22 17:56:40 +0200

    fix: ignore wayfinder generated files for real

--- src/WebShark/resources/js/actions/App/Http/Controllers/index.ts
+++
@@ -1,9 +0,0 @@
-import FileController from './FileController'
-import Settings from './Settings'
-
-const Controllers = {
-    FileController: Object.assign(FileController, FileController),
-    Settings: Object.assign(Settings, Settings),
-}
-
-export default Controllers
