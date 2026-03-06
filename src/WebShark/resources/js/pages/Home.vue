9c9ba8582a333d1f72ee7e795a3ce9e96e18349c
Karolis Zabulis <karolis.zabulis@mif.stud.vu.lt>
2026-03-06 17:01:31 +0200

    UI refactoring
    - Implementing Navbar
    - Changing Home page UI
    
    - Need additional testing related to drag and drop

--- src/WebShark/resources/js/pages/Home.vue
+++ src/WebShark/resources/js/pages/Home.vue
@@ -1,16 +1,23 @@
 <script setup>
     import { ref } from 'vue'
     import { Head, useForm, usePage } from '@inertiajs/vue3'
+    import NavBar from '../components/NavBar.vue'
+
 
     // Information related to the page that comes from inertia (flash messages, csfr_token, etc.)
     const pageMessages = usePage()
 
     // Reacts to changes once the page is loaded
     const file = ref(null);
+    const active = ref(false);
 
     const form = useForm({
         'pcap_file' : null
     })
+
+    function toggleActive(){
+        active.value = !active.value
+    }
 
     // Track file upload 
     function fileChange(event){
@@ -35,6 +42,8 @@
 
 <template>
     <Head title="Home" />
+    <NavBar />
+
     <div class = "flex-auto">
         <!-- Feedback messages -->
         <div v-if="pageMessages.props.flash.error" class="bg-red-100 border border-red-400 text-center text-red-700 px-4 py-3 rounded relative" role="alert">
@@ -45,38 +54,52 @@
         </div>
 
         <div class = "spacer"></div>
-        <!-- Logo -->
-        <div class = "flex justify-center">
-            <h1 class = "WS-title"> WebShark </h1>
+
+        <div class="text-center">
+            <h1 class="title-normal">Analyze your network</h1>
+            <h1 class="title-gradient">traffic in seconds</h1>
         </div>
-        <div class = "text-center py-4 text-lg">
-            <p>Upload network files, analyze packets, visualize data,<br>prevent potential network attacks.</p>
+        <div class="title-info pt-6 pb-20 text-center">
+            <p>Upload a PCAP or PCAPNG capture file and get instant insights <br> into your network packets - protocols, flows and more.</p>
         </div>
+
         <!-- File upload container -->
-        <div class = "file-container">
-            <div class="flex justify-center py-4">
-                <svg xmlns="http://www.w3.org/2000/svg" height="60" fill="currentColor" class="file-icon bi bi-file-earmark-arrow-up-fill" viewBox="0 0 16 16">
-                    <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M6.354 9.854a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 8.707V12.5a.5.5 0 0 1-1 0V8.707z"/>
-                </svg>
+        <div class="flex justify-center pb-15">
+            <div @dragenter.prevent="toggleActive" 
+                @dragleave.prevent="toggleActive" 
+                @dragover.prevent
+                @drop.prevent="toggleActive"
+                :class="{'active-dropzone': active}"
+                class="file-dropbox rounded-md">
+                <form @submit.prevent="submit" enctype="multipart/form-data">
+                    <div class="flex justify-center pt-10">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="60" fill="currentColor" class=" bi bi-file-earmark-arrow-up-fill" viewBox="0 0 16 16">
+                            <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M6.354 9.854a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 8.707V12.5a.5.5 0 0 1-1 0V8.707z"/>
+                        </svg>
+                    </div>
+                    <div class="pt-8 text-center">
+                        <span class="upload-text-primary">Drop your capture file here</span>
+                        <p class="upload-text-secondary pt-1">or click to browse - PCAP & PCAPNG supported up to 10 MB</p>
+                    </div>
+
+                    <div v-if="!file" class="flex py-6 justify-center">
+                        <label class="hover:bg-gray-300 bg-gray-200 cursor-pointer browse-files-btn flex justify-center items-center rounded-md">
+                            Browse files
+                            <input @change="fileChange" type="file" accept=".pcap, .pcapng" class="hidden"/>
+                        </label>
+                    </div>
+                    <div>
+                        <div v-if="file" class="display-file-name pt-4 flex justify-center">
+                            <span>Selected file:  </span>
+                            <p>{{ file.name }}</p>
+                        </div>
+                        <div v-if="file" class="flex justify-center pt-2" >
+                            <button type="submit" class="hover:bg-gray-300 bg-gray-200 browse-files-btn m-2 cursor-pointer border border-solid rounded-md"> Analyze! </button>
+                            <button @click="resetFile" type="button" class="hover:bg-gray-300 bg-gray-200 m-2 browse-files-btn cursor-pointer border border-solid rounded-md "> Reset </button>
+                        </div>
+                    </div>
+                </form>
             </div>
-            <form @submit.prevent="submit" enctype="multipart/form-data">
-                <div class="flex justify-center items-center">
-                    <label v-if="!file" class="file-select-btn cursor-pointer text-center border rounded-md px-4 py-2">
-                        Upload file
-                        <input @change="fileChange" type="file" accept=".pcap, .pcapng" class="hidden"/>
-                    </label>
-                </div>
-                <div v-if="!file" class="upload-text text-center py-5">
-                    <p>PCAP and PCAPNG files are allowed, up to 10MB</p>
-                </div>
-                <div v-if="file" class="display-file-name flex justify-center">
-                    <p>{{ file.name }}</p>
-                </div>
-                <div v-if="file" class="flex justify-center py-4" >
-                    <button type="submit" class="file-upload-btn cursor-pointer border border-solid rounded-md"> Analyze! </button>
-                    <button @click="resetFile" type="button" class="file-upload-btn cursor-pointer border border-solid rounded-md "> Reset </button>
-                </div>
-            </form>
         </div>
         <div class = "footer"></div>
     </div>
