51c5f7f999e9dbc14c9dfab3b9e381892e393214
Šerelis Lukas <lukas.serelis@mif.stud.vu.lt>
2026-03-19 12:49:32 +0200

    update ci/cd
    updated: ci/cd, icon. added: activity diagram

--- src/WebShark/routes/console.php
+++ src/WebShark/routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('app:delete-expired-redis-jobs')->everyMinute();
 Schedule::command('app:delete-expired-ip-markers')->everyMinute();
